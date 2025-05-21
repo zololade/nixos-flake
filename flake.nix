16261cf844cdb0caa5a7c6025a748be215ff4156
Ololade <73138104+zololade@users.noreply.github.com>
2025-05-21 12:46:59 +0100

    fix

--- flake.nix
+++ flake.nix
@@ -7,11 +7,6 @@
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs"; 
 
-  #Add ags as a flake input
-     astal = {
-      url = "github:aylur/astal";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     ags = {
       url = "github:aylur/ags";
       inputs.nixpkgs.follows = "nixpkgs";

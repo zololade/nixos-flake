b5bdee2132638d4bc7cc655632224646a931ced9
Ololade <73138104+zololade@users.noreply.github.com>
2025-05-21 12:18:14 +0100

    fix

--- flake.nix
+++ flake.nix
@@ -1,14 +1,12 @@
-# ~/nixos-flake/flake.nix
 {
-  description = "My NixOS configuration";
+  description = "My NixOS configuration with Astal and AGS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; 
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs"; 
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-  #Add ags as a flake input
-     astal = {
+    astal = {
       url = "github:aylur/astal";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -18,23 +16,29 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, astal, ags, ... }@inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
     nixosConfigurations.ololade = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux"; 
-      specialArgs = { inherit inputs; }; 
+      system = system;
+      specialArgs = { inherit inputs; };
       modules = [
         ./hosts/ololade/configuration.nix
         home-manager.nixosModules.home-manager
       ];
     };
 
-     devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-       packages = with nixpkgs.legacyPackages.x86_64-linux; [
-         git
-         nodejs
-       ];
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = [
+        ags.packages.${system}.default
+        pkgs.wrapGAppsHook
+        pkgs.gobject-introspection
+      ];
 
-     };
+      shellHook = ''
+        export ASTAL3_LIBRARY_PATH=${astal.packages.${system}.astal3}/lib
+      '';
+    };
   };
 }
-

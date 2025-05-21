0834271b64465b619d28b644615de130e2f916d4
Ololade <73138104+zololade@users.noreply.github.com>
2025-05-21 03:11:45 +0100

    ags

--- home-manager/ags.nix
+++ home-manager/ags.nix
@@ -5,15 +5,15 @@
   home.homeDirectory = "/home/ololade";
   home.stateVersion = "23.11"; # or your actual nixos-version
 
-  systemd.user.services.ags = {
-    enable = true;
-    description = "Aylur's GTK Shell";
-    ExecStart = "${pkgs.ags.packages.${pkgs.system}.default}/bin/ags -c ${config.home.homeDirectory}/.config/ags/build/main.js";
-    WantedBy = [ "graphical-session.target" ];
-  };
+  #systemd.user.services.ags = {
+   # enable = true;
+   # description = "Aylur's GTK Shell";
+   # ExecStart = "${pkgs.ags.packages.${pkgs.system}.default}/bin/ags -c ${config.home.homeDirectory}/.config/ags/build/main.js";
+   # WantedBy = [ "graphical-session.target" ];
+  #};
 
-  home.file.".config/ags" = {
-    source = /home/ololade/.config/ags;
-    recursive = true;
-  };
+  #home.file.".config/ags" = {
+   # source = /home/ololade/.config/ags;
+   # recursive = true;
+  #};
 }

ba2c2c31922acfbc6ccd3502f0c6f83886c804ca
Ololade <73138104+zololade@users.noreply.github.com>
2025-05-21 04:47:37 +0100

    Integrate Home Manager configuration for ololade

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; 
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs"; 
   };
 

b14204cbb2a6e157770058d3b3b8e3915a6e9e66
Ololade <73138104+zololade@users.noreply.github.com>
2025-05-21 15:45:38 +0100

    stable

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -3,30 +3,6 @@
 
 {
 
- # add the home manager module
-  #imports = [ inputs.ags.homeManagerModules.default ];
-
-  #programs.ags = {
-  #  enable = true;
-
-    # symlink to ~/.config/ags
-   # configDir = ../ags;
-
-    # additional packages to add to gjs's runtime
-   # extraPackages = with pkgs; [
-    #  inputs.ags.packages.${pkgs.system}.battery
-    #  fzf
-   # ];
-  #};
-
-
-
-
-
-
-
-
-
   # Define your user's name
   home.username = "ololade";
   home.homeDirectory = "/home/ololade"; # Make sure this matches your actual home directory

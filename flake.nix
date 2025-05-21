3370c3aee91cfbf17394539791e7d9f83449ebd4
Ololade <73138104+zololade@users.noreply.github.com>
2025-05-21 12:04:01 +0100

    fix

--- flake.nix
+++ flake.nix
@@ -1,40 +1,71 @@
-# ~/nixos-flake/flake.nix
 {
   description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; 
-    home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs"; 
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-  #Add ags as a flake input
-     astal = {
+    astal = {
       url = "github:aylur/astal";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     ags = {
       url = "github:aylur/ags";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations.ololade = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux"; 
-      specialArgs = { inherit inputs; }; 
+  outputs = inputs@{ self, nixpkgs, home-manager, astal, ags, ... }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations.ololade = pkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {
+        inherit inputs;
+        astal = inputs.astal;
+        ags = inputs.ags;
+      };
       modules = [
         ./hosts/ololade/configuration.nix
         home-manager.nixosModules.home-manager
       ];
     };
 
-     devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-       packages = with nixpkgs.legacyPackages.x86_64-linux; [
-         git
-         nodejs
-       ];
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        git
+        nodejs
+        ags.packages.${system}.default
+      ];
+    };
 
-     };
+    # This is the important bit: your shell bundle package
+    packages.${system}.astal-shell = pkgs.stdenvNoCC.mkDerivation rec {
+      name = "astal-shell";
+      src = ./.;
+
+      nativeBuildInputs = [
+        ags.packages.${system}.default
+        pkgs.wrapGAppsHook
+        pkgs.gobject-introspection
+      ];
+
+      buildInputs = with astal.packages.${system}; [
+        astal3
+        io
+        # Add other Astal libraries as needed
+      ];
+
+      installPhase = ''
+        mkdir -p $out/bin
+        ags bundle app.ts $out/bin/${name}
+      '';
+    };
+
+    # Optionally set the default package to the shell
+    packages.${system}.default = self.packages.${system}.astal-shell;
   };
 }
-

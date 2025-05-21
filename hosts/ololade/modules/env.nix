0c8438ff00d052719859ec0515bd6f8301592658
Ololade <73138104+zololade@users.noreply.github.com>
2025-05-21 21:34:39 +0100

    fix

--- hosts/ololade/modules/env.nix
+++ hosts/ololade/modules/env.nix
@@ -2,7 +2,6 @@
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     QT_QPA_PLATFORM = "wayland";
-    QT_STYLE_OVERRIDE = "adwaita-dark";
     SDL_VIDEODRIVER = "wayland";
     WLR_NO_HARDWARE_CURSORS = "1"; # just in case of cursor bugs
   };

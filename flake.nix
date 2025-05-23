1238c934e4fae0b69520a95688d8c51b6ce4d4df
Ololade <73138104+zololade@users.noreply.github.com>
2025-05-23 03:28:54 +0100

    fix libsecret

--- flake.nix
+++ flake.nix
@@ -21,7 +21,10 @@
 
 
     homeConfigurations.ololade = home-manager.lib.homeManagerConfiguration {
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      pkgs = import nixpkgs { 
+        system = "x86_64-linux"; 
+        config.allowUnfree = true;
+      };
       extraSpecialArgs = { inherit inputs self; };
       modules = [
        ./home-manager/home.nix

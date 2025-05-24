edda64813d325e19dd6379b3603b51d3aac6591a
Ololade <73138104+zololade@users.noreply.github.com>
2025-05-24 04:41:55 +0100

    chaned shtll

--- hosts/ololade/modules/users.nix
+++ hosts/ololade/modules/users.nix
@@ -8,7 +8,7 @@ virtualisation.docker.enable = true;
     isNormalUser = true;
     description = "Ololade";
     extraGroups = [ "wheel" "networkmanager" "video" "audio" "docker" ];
-    # shell = pkgs.bash;
+    shell = pkgs.fish;
     packages = with pkgs; [
       tree
       vlc

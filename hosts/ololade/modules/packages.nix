2260eb37abed048d110fe4609acc16c19d260121
Ololade <73138104+zololade@users.noreply.github.com>
2025-05-25 08:40:44 +0100

    Update packages.nix

--- hosts/ololade/modules/packages.nix
+++ hosts/ololade/modules/packages.nix
@@ -69,6 +69,14 @@
     github-desktop
     upower
     tlp
+    gtk4
+    gtk3
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav
     ];
 
     programs.java = {

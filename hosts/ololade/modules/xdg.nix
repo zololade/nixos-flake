9c0fb7fe81b9fb046242312e9228b31593b99e0e
Ololade <73138104+zololade@users.noreply.github.com>
2025-07-15 22:39:25 +0100

    Update xdg.nix

--- hosts/ololade/modules/xdg.nix
+++ hosts/ololade/modules/xdg.nix
@@ -4,5 +4,4 @@
   xdg.portal.enable = true;
   xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk xdg-desktop-portal-hyprland ];
   xdg.terminal-exec.enable = true;
-  xdg.mime.enable = true;
 }

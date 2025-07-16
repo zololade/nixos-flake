3dd8c99ba0a494afeb733bbb18a445e93d542924
Ololade <73138104+zololade@users.noreply.github.com>
2025-07-16 19:11:07 +0100

    Update services.nix

--- hosts/ololade/modules/services.nix
+++ hosts/ololade/modules/services.nix
@@ -14,7 +14,7 @@
     extraConfig.pipewire = {
       "context.properties" = {
         "default.clock.rate" = 48000;
-        "default.clock.quantum" = 256;
+        "default.clock.quantum" = 128;
         "default.clock.min-quantum" = 32;
         "default.clock.max-quantum" = 4096;
       };

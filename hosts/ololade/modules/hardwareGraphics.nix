64b0168a12969ade553d10f870b3ec7b7e795cb3
Ololade <73138104+zololade@users.noreply.github.com>
2025-07-16 11:33:06 +0100

    Update hardwareGraphics.nix

--- hosts/ololade/modules/hardwareGraphics.nix
+++ hosts/ololade/modules/hardwareGraphics.nix
@@ -2,19 +2,19 @@
 
 {
  
-  #hardware.graphics.enable = true;     
-  #hardware.graphics.enable32Bit = true;    
+  hardware.graphics.enable = true;     
+  hardware.graphics.enable32Bit = true;    
 
-  #hardware.graphics.extraPackages = with pkgs; [
-    #intel-media-driver   
-    #intel-vaapi-driver   
-    #libva-utils          
-    #intel-ocl
-  #];
+  hardware.graphics.extraPackages = with pkgs; [
+    intel-media-driver   
+    intel-vaapi-driver   
+    libva-utils          
+    intel-ocl
+  ];
 
-  #hardware.graphics.extraPackages32 = with pkgs; [
-    #intel-media-driver   
-    #intel-vaapi-driver   
-  #];
+  hardware.graphics.extraPackages32 = with pkgs; [
+    intel-media-driver   
+    intel-vaapi-driver   
+  ];
 
 }
